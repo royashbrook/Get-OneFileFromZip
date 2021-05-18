0a9ec8df242980e8ed5c078d5c67bc218ce8204c
Roy Ashbrook <rashbrook@patriottrans.com>
2021-05-18 15:52:23 -0400

    add sheet name support

--- Get-OneFileFromZip/Get-OneFileFromZip.psm1
+++ Get-OneFileFromZip/Get-OneFileFromZip.psm1
@@ -8,4 +8,18 @@ function Get-OneFileFromZip($Path,$File){
     $null=$stream.Dispose()
     $null=$zip.Dispose()
 }
-Export-ModuleMember -Function Get-OneFileFromZip
+function Get-SheetByName($Path,$SheetName){
+
+    # see the following link for details on spreadsheetml structure
+    # https://docs.microsoft.com/en-us/office/open-xml/structure-of-a-spreadsheetml-document
+    
+    # get internal files that hold sheet mappings
+    [xml]$sheets = Get-OneFileFromZip $Path 'workbook.xml'
+    [xml]$rels = Get-OneFileFromZip $Path 'workbook.xml.rels'
+    #resolve internal file name for sheet by name
+    $rid = ($sheets.workbook.sheets.sheet | Where-Object name -eq $SheetName).id
+
+    $sheetfile = split-path -leaf ($rels.Relationships.Relationship | Where-Object id -eq $rid).Target
+    Get-OneFileFromZip $Path $sheetfile
+ }
+Export-ModuleMember -Function Get-OneFileFromZip,Get-SheetByName
